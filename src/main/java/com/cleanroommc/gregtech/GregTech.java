7253e0a42eb4dd357ccdf1f58a748e40b779265e
Rongmario <rongmario@protonmail.com>
2022-09-08 18:18:28 +0100

    Meta stuff

---
+++ src/main/java/com/cleanroommc/gregtech/GregTech.java
@@ -0,0 +1,12 @@
+package com.cleanroommc.gregtech;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = GregTech.ID, name = GregTech.NAME, version = GregTech.VERSION)
+public class GregTech {
+
+    public static final String ID = "gregtech";
+    public static final String NAME = "GregTech Intergalactical";
+    public static final String VERSION = "0.0.1";
+
+}
